b367aa0d72015d95df8e7dbde5193ad411436bd9
Konstantin Web <47927228+ykosweb@users.noreply.github.com>
2021-12-23 19:08:30 +0200

    fix write form

--- components/WriteForm/index.tsx
+++ components/WriteForm/index.tsx
@@ -1,5 +1,6 @@
-import {Input} from "@material-ui/core";
-import classes from "./WriteForm.module.scss"
+import {Button, Input} from "@material-ui/core";
+import classes from "./WriteForm.module.scss";
+import MessageIcon from '@material-ui/icons/TextsmsOutlined';
 import clsx from "clsx";
 
 interface WriteFormProps {
@@ -7,6 +8,8 @@ interface WriteFormProps {
 }
 
 const WriteForm: React.FC<WriteFormProps> = ({title}) => {
+
+
     return (
         <div className={classes.writeForm}>
             <Input
@@ -15,10 +18,16 @@ const WriteForm: React.FC<WriteFormProps> = ({title}) => {
                 className={clsx(classes.field, classes.fieldTitle)}
             />
             <Input
-                placeholder={"Ваша запись"}
+                placeholder={"Текст вашего поста"}
                 defaultValue={title}
-                className={classes.field}
+                className={clsx(classes.field, classes.fieldPost)}
+                multiline
+                minRows={5}
             />
+            <Button variant={"contained"} color={"primary"} className={classes.writeFormBtn}>
+                <MessageIcon className={"mr-10"}></MessageIcon>
+                Опубликовать
+            </Button>
         </div>
     )
 }
